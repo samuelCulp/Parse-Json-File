d1f4aa6ff34e505db408eee752610af571fbb3b6
samuelCulp <99611615+samuelCulp@users.noreply.github.com>
2022-11-03 20:42:44 -0500

    Update getProID.java

--- getProID.java
+++ getProID.java
@@ -1,92 +1,47 @@
+import java.util.Scanner;
 import java.io.File;  
 import java.io.FileNotFoundException;  
 import java.util.Scanner;
 
-/**
- * use this to get all of the property_id numbers 
- * happy birthday
- */
+public class getProID2  {
 
-
-public class getProID {
-
-
-    static String parseString(String temp){
-        temp = temp.replace("\", \"", "" );
-         temp = temp.replace("\"", "");
-         temp = temp.replace(",", "");
-        //  temp = temp.replace("property_id:", "");
-        //  temp = temp.replace("\s", "");
-         return temp;
-     }
-
-    static String stop(String words){
-        String temp = "";
-        for (int i = 0; i < words.length(); i++){
-            temp += words.charAt(i);
-            if (words.charAt(i) == ','){
-                break;
-            }
-        }
-        return temp;
+    public static String ParseStringQualifications(String text) {
+        // text = StringUtility.ParseID(text);
+        var tmp = text.replace("\",\"", "|");
+        tmp = tmp.replace("\"", "");
+        tmp = tmp.replace(",", "");
+        tmp = tmp.replace("property_id:", "");
+        tmp = tmp.replace(" ", "");
+        return tmp;
     }
-
-    /**
-     * @param data
-     * @return
-     */
-    static String find(String data){
-        
-
+    
+    static void findStringQualifications(String data){
 
         try{
-        String find = "\"property_id\":";
-        int i = data.indexOf(find);
-        
-        if(i>0){
+            String find = "\"property_id\":";
+            int i = data.indexOf(find);
 
-            String newData = (data.substring(data.indexOf(find)));
-            // System.out.println(i);
-            String parsedData = stop(newData);
-            data = parseString(parsedData);
-            System.out.println(data);
-              
-        }
-        else {
-            // System.out.println("not found");
-        }
-    } 
-    catch(Exception e){
-        System.out.print("SOL");
-    }
-
-        return data;
-    }
-    
-    public static void main(String[] args) throws Exception 
-    {
-        // AddressType add = new AddressType();
-       String data = "";
-        try {
-            File ReadJsonFile = new File("jsonListedHelena.json");
-            Scanner JsonFile = new Scanner(ReadJsonFile);
-
-            while (JsonFile.hasNextLine()){
-                String line = JsonFile.nextLine();
-                String newLine = find(line);
-                data += line;
-                // System.out.println(newLine);
+            if(i>0){
+                String newData = (data.substring(i));
+                newData = ParseStringQualifications(newData);
+                System.out.println(newData);
             }
-            // data = find(data);
-            // add.City = data;
-            // System.out.println(data);
-            JsonFile.close();
+            
+        } catch(Exception e){
+            System.out.println("not found");
         }
-        catch (FileNotFoundException e) {
-            System.out.println("no good");
-            e.printStackTrace();
-        }
-    
+       
     }
+    
 
+    public static void main(String[] args) throws Exception{
+        File ReadJsonFile = new File("jsonListedHelena.json");
+        Scanner JsonFile = new Scanner(ReadJsonFile);
+
+        while(JsonFile.hasNextLine()){
+            String line = JsonFile.nextLine();
+            findStringQualifications(line);
+        }
+        JsonFile.close();
+    }
 }
